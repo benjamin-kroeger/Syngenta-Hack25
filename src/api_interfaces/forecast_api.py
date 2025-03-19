0fbec0edd9f7c8a9e6b5b3a70e3b50088d396985
Benjamin Bjoern Kroeger <benjamin_kroeger@t-online.de>
2025-03-20 00:37:03 +0100

    improve forcast api interaction

--- src/api_interfaces/forecast_api.py
+++ src/api_interfaces/forecast_api.py
@@ -1,36 +1,73 @@
+from typing import Any
+
 import requests
 import os
 from datetime import datetime, timedelta
 import pandas as pd
 
 base_url = "https://services.cehub.syngenta-ais.com"
+api_key = "4a8a11bc-2c85-41f5-9a68-13965191123e"
 
-def reqeust_daily_forecast(logitude: float, latitude:float,date:datetime,number_of_days):
-    assert date > datetime.now(), "The requested date must be in the future"
 
-    endpoint = "/api/Forecast/ShortRangeForecastDaily"
+def query_forecast_api_general(
+        longitude: float,
+        latitude: float,
+        start_date: datetime,
+        end_date: datetime,
+        measurement_label: list[str],
+        endpoint: str,
+        top: int = 100
+) -> dict | None:
+    assert isinstance(start_date, datetime), "The start date must be python datetime format"
+    assert isinstance(end_date, datetime), "The end date must be python datetime format"
+
+
     params = {
-        "longitude":logitude,
-        "latitude":latitude,
-        "startDate": date.strftime("%Y-%m-%d"),
-        "endDate": (date + timedelta(days=number_of_days)).strftime("%Y-%m-%d"),
+        "longitude": longitude,
+        "latitude": latitude,
+        "startDate": start_date.strftime("%Y-%m-%d"),
+        "endDate": end_date.strftime("%Y-%m-%d"),
         "supplier": "Meteoblue",
-        "measureLabel": "TempAir_DailyMin (C)",
-        "top": 5,
+        "measureLabel": ";".join(measurement_label),
+        "top": top,
         "format": "json"
     }
     headers = {
-        "ApiKey": "4a8a11bc-2c85-41f5-9a68-13965191123e"
+        "ApiKey": api_key,
     }
 
     response = requests.get(base_url + endpoint, headers=headers, params=params)
 
-    forecast_df = pd.DataFrame(data=response.json())
     if response.status_code == 200:
-        return forecast_df
+        return response.json()
     else:
         response.raise_for_status()
 
 
+def reqeust_daily_forecast(longitude: float, latitude: float, date: datetime, number_of_days:int):
+    """
+    Get the daily forecast for a given date and number of days.
+    :param longitude:
+    :param latitude:
+    :param date:
+    :param number_of_days:
+    :return:
+    """
+
+    assert date > datetime.now(), "The requested date must be in the future"
+
+    api_query_results = query_forecast_api_general(
+        longitude=longitude,
+        latitude=latitude,
+        start_date=date,
+        end_date=date + timedelta(days=number_of_days),
+        measurement_label=["TempAir_DailyMin (C)"],
+        endpoint="/api/Forecast/ShortRangeForecastDaily"
+    )
+
+    forecast_df = pd.DataFrame(data=api_query_results)
+    return forecast_df
+
+
 if __name__ == "__main__":
-    print(reqeust_daily_forecast(logitude=7,latitude=14,date=datetime.today() + timedelta(days=4), number_of_days=5))
+    print(reqeust_daily_forecast(longitude=7, latitude=14, date=datetime.today() + timedelta(days=4), number_of_days=5))
